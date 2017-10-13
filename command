50d5b74a8834b853a48cbdf223c90a1d53e27233
antons <anton.shestakov2@gmail.com>
2017-10-13 22:47:58 +0300

    initial version

---
+++ command
@@ -0,0 +1,1 @@
+deploy-contracts --deployment-file=crowdsales/golfco.yml --deployment-name=testnet --address=
---
+++ deploy_contracts.sh
@@ -0,0 +1,2 @@
+source venv/bin/activate
+deploy-contracts --deployment-file=crowdsales/golfco.yml --deployment-name=mainnet --address=0x0066F57959c75Dd9a2eAD293bAF7794c569C43fC
